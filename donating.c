6f07c8417379499b2aeb4b0f0873be0a17005778
vishwanath377 <61659771+vishwanath377@users.noreply.github.com>
2021-01-26 10:30:01 +0530

    donating.c

---
+++ donating.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+  int orphan,oldage,disaster,choice;
+  while(choice!=4)
+  {
+       printf("\n press 1 for donating to orphan");
+       printf("\n press 2 for donating to oldage");
+       printf("\n press 3 for donating to natural disaster victims");
+       printf("\n enter your choice");
+       scanf("%d",&choice);
+       switch(choice)
+       {
+        case 1:
+              printf("enter amount u want to donate for orphan");
+              scanf("%d",&orphan);
+              printf("\n Thanks for donating %d amount");
+              break;
+        case 2:
+              printf("\n enter amount u want to donate for oldage");
+              scanf("%d",&oldage);
+              printf("\n Thanks for donating %d amount");
+              break;
+        case 3:
+              printf("\n enter amount u want to donate for natural disaster victims");
+              scanf("%d",&disaster);
+              printf("\n Thanks for donating %d amount");
+              break;
+        default:
+              ("Sorry u have entered wrong one");
+       }
+   }
+}   
